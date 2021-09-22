42b3d39cdc8a2452eabc2ac24987d27cb7cd7ea9
chamRun <chrm@aut.ac.ir>
2021-09-22 12:22:38 +0330

    working on ShmareRnd...

--- C++/ShmareRnd.cpp
+++ C++/ShmareRnd.cpp
@@ -6,23 +6,52 @@ using namespace std;
 
 bool has_four_digits(string number)
 {
-    number = "changed";
-    /*
-    int counter = 0;
-    for (size_t j = 0; j < number.length(); j++)
-    {
-        char to_be_counted = number.at(j);
 
-        for (size_t k = ; k < count; k++)
+    for (size_t i = 0; i < number.length() - 2; i++)
+    {
+        if (number.at(i) == '!')
+            continue;
+
+        int counter = 0;
+        for (size_t j = i + 1; j < number.length(); j++)
         {
+            if (number.at(j) == number.at(i))
+            {
+                number.at(j) == '!';
+                counter++;
+                if (counter == 3)
+                    return true;
+            }
         }
+
+        number.at(i) = '!';
     }
 
+    return false;
+}
 
-    */
+bool has_three_digits(string number)
+{   
+    //cout << "started...";
+    for (size_t i = 0; i < number.length() - 2; i++)
+    {
+        //cout << number.at(i) << number.at(i + 1) << number.at(i + 2) << ".\n";
+        if (number.at(i) == number.at(i + 1) && number.at(i) == number.at(i + 2))
+            return true;
+    }
+
+    return false;
+}
+
+bool is_mirror(string number)
+{
+    for (size_t j = 0; j < number.length() / 2; j++)
+    {
+        if(number.at(j) != number.at(number.length() - 1 - j))
+            return false;
+    }
 
     return true;
-
 }
 
 int main()
@@ -40,9 +69,13 @@ int main()
     {
 
         if (has_four_digits(numbers[i]))
-        {
-            
-        }
+            cout << "Ronde!";
+        else if (has_three_digits(numbers[i]))
+            cout << "Ronde!";
+        else if (is_mirror(numbers[i]))
+            cout << "Ronde!";
+        else
+            cout << "Rond nist.";
     }
 
     return 0;
