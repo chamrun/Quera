bf136ad03c53c9db16d4a4e1b3ba6ecce6d62719
chamRun <chrm@aut.ac.ir>
2021-09-26 14:59:35 +0330

    working in DzdPrnde

--- C++/DzdPrnde.cpp
+++ C++/DzdPrnde.cpp
@@ -2,35 +2,8 @@
 
 #include <iostream>
 #include <map>
-#include <unordered_map>
-#include <bits/stdc++.h>
 
 using namespace std;
-
-void swap(int *xp, int *yp)
-{
-    int temp = *xp;
-    *xp = *yp;
-    *yp = temp;
-}
-
-void selectionSort(int arr[], int n)
-{
-    int i, j, min_idx;
- 
-    // One by one move boundary of unsorted subarray
-    for (i = 0; i < n-1; i++)
-    {
-        // Find the minimum element in unsorted array
-        min_idx = i;
-        for (j = i+1; j < n; j++)
-        if (arr[j] < arr[min_idx])
-            min_idx = j;
- 
-        // Swap the found minimum element with the first element
-        swap(&arr[min_idx], &arr[i]);
-    }
-}
  
 
 
@@ -57,6 +30,13 @@ int main()
             cin >> police_side;
             polices[police_floor] = police_side;
         }
+
+        for (size_t i = 0; i < n_polices; i++)
+        {
+            cout << polices.at(i);
+        }
+        
+
     }
 
     return 0;
