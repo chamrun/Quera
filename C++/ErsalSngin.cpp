c7236b601178c4b976a54898ae7ae1ea96d01ead
chamRun <chrm@aut.ac.ir>
2021-10-27 21:54:54 +0330

    hei

---
+++ C++/ErsalSngin.cpp
@@ -0,0 +1,27 @@
+// 0 / 100
+
+#include <iostream>
+#include <map>
+
+using namespace std;
+ 
+
+
+int main()
+{
+
+    int n;
+    cin >> n;
+
+    int m;
+    cin >> m;
+
+    for (size_t i = 0; i < n; i++)
+    {
+        
+    }
+    
+
+
+    return 0;
+}
