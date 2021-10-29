d22e353673d24b8e2b61cec15930c24330623bcc
chamRun <chrm@aut.ac.ir>
2021-10-29 20:11:44 +0330

    Update solution.py

--- Python/Technology/ShtrnjMivei/solution.py
+++ Python/Technology/ShtrnjMivei/solution.py
@@ -1,1 +1,21 @@
-def fruits(fruit_list):
+def is_good(fruit: dict):
+    if fruit.get('shape') is 'sphere'
+        and fruit.get('mass') <  
+        and fruit.get('shape') is 'sphere' and
+    pass
+
+
+def fruits(fruit_list: tuple):
+
+
+
+    count = 0
+
+    for fruit in fruit_list:
+        if is_good(fruit):
+            count += 1
+
+
+
+
+    return {'apple': 2, 'lemon': 1}
