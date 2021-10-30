c22bf5b41a94b273fa7a036f58530ff5cd250963
chamRun <chrm@aut.ac.ir>
2021-10-30 14:33:54 +0330

    NzrieRisman 100/100

---
+++ Python/Technology/NzrieRisman.py
@@ -0,0 +1,15 @@
+
+
+def main():
+    a_str = input()
+    b_str = input()
+
+    if b_str.__contains__(a_str):
+        print("1")
+
+    else:
+        print("0")
+
+
+if __name__ == '__main__':
+    main()
